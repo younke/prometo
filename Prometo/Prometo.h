0151201dec218e9632007bb379e7cb85ebb4e6b8
Vasily Ptitsyn <kewgram@gmail.com>
2018-10-28 12:08:16 +0900

    Initial Commit

---
+++ Prometo/Prometo.h
@@ -0,0 +1,19 @@
+//
+//  Prometo.h
+//  Prometo
+//
+//  Created by younke on 28/10/2018.
+//  Copyright © 2018 funjahmental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prometo.
+FOUNDATION_EXPORT double PrometoVersionNumber;
+
+//! Project version string for Prometo.
+FOUNDATION_EXPORT const unsigned char PrometoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prometo/PublicHeader.h>
+
+
---
+++ PrometoTests/PrometoTests.swift
@@ -0,0 +1,34 @@
+//
+//  PrometoTests.swift
+//  PrometoTests
+//
+//  Created by younke on 28/10/2018.
+//  Copyright © 2018 funjahmental. All rights reserved.
+//
+
+import XCTest
+@testable import Prometo
+
+class PrometoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
